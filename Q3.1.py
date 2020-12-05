ecb794d524ef5d6688a2505a06d9ab55ea63c4b3
BilalGunden-Insider <73845741+BilalGunden-Insider@users.noreply.github.com>
2020-12-05 15:33:32 +0300

    fix

--- Q3.1.py
+++ Q3.1.py
@@ -1,36 +1,43 @@
 class Ogrenci:
-    def __init__(self, ogrenciAdi, ogrenciSoyadi, ogrenciSinifi):
-        self.ogrenciAdi = ogrenciAdi
-        self.ogrenciSoyadi = ogrenciSoyadi
-        self.ogrenciSinifi = ogrenciSinifi
+    def __init__(self, ogrenci_adi, ogrenci_soyadi, ogrenci_sinifi):
+        self.ogrenci_adi = ogrenci_adi
+        self.ogrenci_soyadi = ogrenci_soyadi
+        self.ogrenci_sinifi = ogrenci_sinifi
 
     def ogrenciBilgiGoster(self):
-        print("Adı : {} \n Soyadı : {} \n Sınıfı : {}".format(self.ogrenciAdi, self.ogrenciSoyadi, self.ogrenciSinifi))
+        print(
+            "Adı : {} \n Soyadı : {} \n Sınıfı : {}".format(self.ogrenci_adi, self.ogrenci_soyadi, self.ogrenci_sinifi))
 
 
 class Soru:
     def __init__(self):
-        self.dogruSayisi = int(input("Öğrenci doğru sayısı :"))
-        self.yanlisSayisi = int(input("Öğrenci yanlış sayısı :"))
+        while True:
+            self.dogru_sayisi = int(input("Öğrenci doğru sayısı :"))
+            self.yanlis_sayisi = int(input("Öğrenci yanlış sayısı :"))
+            toplam_soru = self.dogru_sayisi + self.yanlis_sayisi
+            if toplam_soru > 50:
+                print("Toplam soru sayısı 50'den fazla olamaz.")
+                continue
+            else:
+                # we're happy with the value given.
+                # we're ready to exit the loop.
+                break
 
     def netSayisi(self):
-        toplamSoru = self.dogruSayisi + self.yanlisSayisi
-        if toplamSoru > 100:
-            print("Toplam soru sayısı 100'den fazla olamaz.")
-            quit()
 
-        gidenDogruSayisi = self.yanlisSayisi / 4
-        netSayisi = self.dogruSayisi - gidenDogruSayisi
-        return netSayisi
+        net_sayisi = self.dogru_sayisi - (self.yanlis_sayisi / 4)
+        print("Öğrenci net sayısı : {}".format(net_sayisi))
+        return net_sayisi
 
-    def puanHesaplama(self, netSayisi):
-        ogrenciPuan = netSayisi * 10
-        if ogrenciPuan < 0:
-            ogrenciPuan = 0
-        return ogrenciPuan
+    def puanHesaplama(self, net_sayisi):
+        ogrenci_puan = net_sayisi * 2
+        return ogrenci_puan
 
 
-ogrenciExample = Ogrenci("Bilal", "Günden", "11/C")
+ad = input("AD: ")
+soyad = input("SOYADI: ")
+sinif = input("SINIF: ")
+ogrenciExample = Ogrenci(ad, soyad, sinif)
 Soru1 = Soru()
 ogrenciExample.ogrenciBilgiGoster()
-print(Soru1.puanHesaplama(Soru1.netSayisi()))
+print("Öğrenci Puanı : {}".format(Soru1.puanHesaplama(Soru1.netSayisi())))
